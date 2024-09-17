553b955ad15f3e5780b785d26b612eae68dee53c
Nina Breznik <ninabreznik@gmail.com>
2024-09-17 18:59:29 +0100

    Add copy wsUri and response to ws client

--- app.js
+++ app.js
@@ -7,48 +7,21 @@ async function start () {
   wss.on('connection', async (ws) => {
     console.log('connected')
     ws.on('message', function message(data) {
-      console.log('received: %s', data);
+      console.log('received: %s', data)
+      const res = {
+        type: 'message',
+        text: 'Nice to meet you'
+      }
+      ws.send(JSON.stringify(res))
     });
   });
   
   const wsUri = 'ws://localhost:8080'
-  const clientsocket = new WebSocket(wsUri)
-  clientsocket.onopen = async () => {
-    console.log('socket opened')
-    const msg = {
-      type: 'message',
-      text: 'hello there'
-    }
-    clientsocket.send('something');
-  }
-
-
-
-
-  // const wsUri = "http://192.168.1.182:9966/"
-  // const wsUri = "http://localhost:9342/"
-  // const websocket = new WebSocket(wsUri)
-  // window.ws = websocket
-  // console.log({websocket})
-  // websocket.onopen = async () => {
-  //   console.log('socket opened')
-  //   const msg = {
-  //     type: 'message',
-  //     text: 'hello there'
-  //   }
-  //   websocket.send(JSON.stringify(msg))
-  // }
-  // websocket.onmessage = (e) => {
-  //   console.log('onmessage')
-  //   const msg = JSON.parse(e.data)
-  //   console.log('new message', msg.type)
-  // }
-  // websocket.onerror = (err) => {
-  //   console.log({err})
-  // }
-  // websocket.onclose = (err) => {
-  //   console.log('socket closed')
-  // }
+  const el = document.querySelector('h2')
+  el.addEventListener('click', (e) => { 
+    el.innerHTML = `Copied ${wsUri}`
+    navigator.clipboard.writeText(wsUri);
+  })
 }
 
 start()
