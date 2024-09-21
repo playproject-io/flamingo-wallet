6737e1e4bce5680de1eaec34b4e209282121c595
Nina Breznik <ninabreznik@gmail.com>
2024-09-21 21:39:42 +0100

    Start a worker and get data through the duplex stream

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const { WebSocketServer } = require('ws')
+const b4a = require('b4a')
 
 async function start () {
   document.querySelector('h1').addEventListener('click', (e) => { location = location })
-  
+  console.log({Pear})
+  // console.log({worker: Pear.config.links.worker})
+  // const pipe = Pear.worker.run(Pear.config.links.worker)
+  const pipe = Pear.worker.run('../core-lightning/index.js')
+  pipe.on('data', (data) => {
+    console.log('pipe on data', b4a.toString(data, 'utf-8'))
+  })
+  pipe.on('err', (err) => {
+    console.log('pipe on err', err)
+  })
+  console.log({pipe})
   const wss = new WebSocketServer({ port: 8080 });
   wss.on('connection', async (ws) => {
     console.log('connected')
